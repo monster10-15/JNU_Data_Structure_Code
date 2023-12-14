275e00f3b1d52807fac29e8dda59f623c2ecac22
Phemie <17575010152@163.com>
2023-12-14 21:04:18 +0800

    update

--- .vscode/JNU代码题/Test/Test_13_冒泡排序.c
+++ .vscode/JNU代码题/Test/Test_13_冒泡排序.c
@@ -2,21 +2,23 @@ typedef struct{
     int key;
     infotype otherinfo;
 }Node;
+
 void bsort (Node a[], int n)
-    { NODE temp;
-    int i,j,flag;
-    for(j=1;j<=n;j++){
-        flag=0;
-        for(i=1;i>j;i++){
-            if(a[i].key>a[i+1].key){
-                flag=1;
-                temp=a[i];
-                a[i]=a[i+1];
-                a[i+1]=temp;
+{ 
+    Node temp;
+    int i, j, flag;
+    for(j = 1; j <= n; j++){
+        flag = 0;
+        for(i = 1; i <= n - j; i++){
+            if(a[i].key > a[i+1].key){
+                flag = 1;
+                temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
             }
-            if(flag==0){
-                break;
-            }
+        }
+        if(flag == 0){
+            break;
         }
     }
 }
