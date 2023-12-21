e0c9c36359d3b07985255a1f48c05526c05185eb
Phemie <17575010152@163.com>
2023-12-21 22:53:28 +0800

    更新直接插入排序代码

--- .vscode/JNU代码题/不背会寄的代码题/排序/直接插入排序.c
+++ .vscode/JNU代码题/不背会寄的代码题/排序/直接插入排序.c
@@ -1,26 +1,15 @@
-void insertionSort(Node **head_ref) {
-    // 初始化排序后的链表
-    Node *sorted = NULL;
-    // 遍历给定的链表并插入到排序后的链表中
-    Node *current = *head_ref;
-    while (current != NULL) {
-        // 保存下一个用于后续遍历
-        Node *next = current->next;
-        // 把当前节点插入到排序后的链表中
-        if (sorted == NULL || sorted->data >= current->data) {
-            current->next = sorted;
-            sorted = current;
-        } else {
-            Node *temp = sorted;
-            while (temp->next != NULL && temp->next->data < current->data) {
-                temp = temp->next;
-            }
-            current->next = temp->next;
-            temp->next = current;
+void straightforwardSort(LinkList L){
+    LinkList *p, *pre, *q; // 定义三个指针，p用于遍历链表，pre用于找到插入位置的前一个节点，q用于保存p的下一个节点
+    p=L->next->next; // 从第二个节点开始排序，因为单个节点可以看作已排序
+    L->next->next = NULL; // 将第一个节点独立出来，作为已排序部分
+    while(p!=NULL){ // 遍历未排序部分
+        q=p->next; // 保存p的下一个节点
+        pre=L; // 从头开始找插入位置
+        while(pre->next!=NULL && pre->next->data < p->data){ // 找到第一个大于p的节点
+            pre=pre->next; // 更新pre
         }
-        // 更新当前节点为下一个节点
-        current = next;
+        p->next = pre->next; // 插入p
+        pre->next = p;
+        p=q; // p指向下一个待排序节点
     }
-    // 更新头节点为排序后的链表的头节点
-    *head_ref = sorted;
 }
