a1d49e0d8c3f7b3517b20e024ec2b333cefe29a9
Phemie <17575010152@163.com>
2023-12-22 16:56:11 +0800

    更新

--- .vscode/JNU代码题/不背会寄的代码题/图/√图的邻接表&邻接矩阵的结构体定义.c
+++ .vscode/JNU代码题/不背会寄的代码题/图/√图的邻接表&邻接矩阵的结构体定义.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #define MAX 100
 
-// 定义顶点类型
-typedef char VertexType;
-
 // 边表节点
-typedef struct ArcNode {
+typedef struct ArcNode{
     int adjvex; // 该弧所指向的顶点的位置
     struct ArcNode *nextarc; // 指向下一条弧的指针
 } ArcNode;
 
 // 顶点表节点
-typedef struct VNode {
-    VertexType data; // 顶点信息
+typedef struct VNode{
+    int data; // 顶点信息
     ArcNode *firstarc; // 指向第一条依附该顶点的弧的指针
 } VNode, AdjList[MAX];
 
 // 邻接表
-typedef struct {
+typedef struct ALGraph{
     AdjList vertices; // 邻接表
     int vexnum, arcnum; // 图的当前顶点数和弧数
 } ALGraph;
 
 // 邻接矩阵
-typedef struct {
+typedef struct MGraph{
     int vexnum; // 顶点数
     int edgnum; // 边数
     int matrix[MAX][MAX]; // 邻接矩阵
